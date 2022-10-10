fd7820ff0e096ec11388db6fb497ebaa33160108
tiwari0006 <sbtiwari98011@gmail.com>
2022-10-10 16:28:36 +0530

    program of day 1&2

---
+++ introofvar.js
@@ -0,0 +1,45 @@
+1. // Print "Masai School" in the console followed by "A Transformation in Education" in next line.
+
+
+console.log("Masai School");
+console.log("A Transformation in Eduction")
+
+
+
+2. //Define a variable called name with your Name as the assigned value
+//Print the value stored in the variable name
+//Change the variable to store your father's name
+//Print the value stored in the variable name
+//Change the variable again to store your mother's name.
+//Print the value stored in the variable name
+
+
+let name = "vivek";
+let name2 = "sb tiwari";
+let name3 = "rina devi";
+
+console.log(name2)
+
+
+
+3. //Create a variable name to store your name
+//Create a variable age to store your age
+//Print the values stored in the variable on one line followed by the type of the variable in the next line
+
+let name = "vivek";
+let age = 20;
+
+console.log(name)
+console.log(age)
+
+4.//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+//Print the report card of the student.
+
+let name = "vvk"
+let school = "ccs"
+let grade = "12th"
+let section = "c"
+let roll = 20
+let marks = 294
+
+console.log(name, school, grade, section, roll, marks)
---
+++ mathoperator.js
@@ -0,0 +1,10 @@
+1. //find the product of this values
+//one, two, three, four, five
+
+console.log(1*2*3*4*5)
+
+2. // multiply the all value by 2
+let num = (1+2+3+4+5)
+console.log(num*2)
+
+
